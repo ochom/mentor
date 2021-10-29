9d59524fa5a9735181fcb1dfc74ef772694a4f4f
ochom <ochomrichard752@gmail.com>
2021-10-29 11:56:26 +0300

    fix: code flow

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const path = require("path");
-const app = express(); // create express app
+
+const app = express();
+
+let port = process.env.PORT;
 
 // add middlewares
 app.use(express.static(path.join(__dirname, "..", "build")));
@@ -10,8 +13,6 @@ app.use((req, res, next) => {
   res.sendFile(path.join(__dirname, "..", "build", "index.html"));
 });
 
-let port = process.env.PORT;
-// start express server on port 5000
 app.listen(port, () => {
   console.log(`server started on port: ${port}`);
 });
